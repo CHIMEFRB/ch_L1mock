74f838e2408c73a5a3978b274e74780ff7e1eeff
Kiyoshi Masui <kiyo@physics.ubc.ca>
2016-04-11 12:56:49 -0400

    Reference integrator works.

--- ch_L1mock/L0.py
+++ ch_L1mock/L0.py
@@ -24,6 +24,32 @@ logger = logging.getLogger(__name__)
 class ReferenceIntegrator(ch_vdif_assembler.processor):
 
     def __init__(self, nsamp_integrate=512, **kwargs):
-        super(self, ReferenceIntegrator).__init__(**kwargs)
+        super(ReferenceIntegrator, self).__init__(**kwargs)
         self._nsamp_integrate = nsamp_integrate
+
+    def process_chunk(self, t0, nt, efield, mask):
+        ninteg = self._nsamp_integrate
+        if nt % ninteg:
+            msg = ("nsamp_integrate (%d) must evenly divide number of"
+                   " samples (%d).")
+            msg = msg % (ninteg, nt)
+            raise ValueError(msg)
+        e_squared = abs(efield)**2
+        shape = efield.shape
+        new_shape = shape[:-1] + (shape[-1] // ninteg, ninteg)
+        e_squared.shape  = new_shape
+        mask.shape = new_shape
+
+        # Integrate.
+        intensity = np.sum(e_squared, -1, dtype=np.float32)
+        weight = np.sum(mask, -1, dtype=np.float32)
+        # Normalize for missing data.
+        bad_inds = weight == 0
+        weight[bad_inds] = 1
+        intensity *= ninteg / weight
+        # Convert weight to integer between 0 and 255.
+        weight *= 255 / ninteg
+        weight = np.round(weight).astype(np.uint8)
+        weight[bad_inds] = 0
+
 
--- ch_L1mock/tests/test_L0.py
+++ ch_L1mock/tests/test_L0.py
@@ -2,15 +2,15 @@ import unittest
 
 import ch_vdif_assembler
 
-from .. import L0
+from ch_L1mock import L0
 
 
 class TestReferenceIntegrator(unittest.TestCase):
 
     def test_runs(self):
-        stream = ch_vdif_assembler.make_simulated_stream(nsec=5)
-        assembler = ch_vdif_assembler()
-        p = L0.ReferenceIntegrator(nsamp_integrate=512, is_critical=True)
+        stream = ch_vdif_assembler.make_simulated_stream(gbps=1., nsec=5)
+        assembler = ch_vdif_assembler.assembler()
+        p = L0.ReferenceIntegrator(nsamp_integrate=512)
         assembler.register_processor(p)
         assembler.run(stream)
 
